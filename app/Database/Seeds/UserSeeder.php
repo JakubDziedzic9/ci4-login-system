9d7eddcf3da4be3ecb2de14733676ae9fa769959
Jakub Dziedzic <jakubdziedzic9@gmail.com>
2025-04-25 17:23:00 +0200

    Step 1 - Project setup with database, users table migration and user seeder

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'email'         => 'admin@example.com',
+            'password_hash' => password_hash('haslo123', PASSWORD_DEFAULT),
+            'first_name'    => 'Admin',
+            'created_at'    => date('Y-m-d H:i:s'),
+            'updated_at'    => date('Y-m-d H:i:s'),
+        ];
+
+        $this->db->table('users')->insert($data);
+    }
+}
